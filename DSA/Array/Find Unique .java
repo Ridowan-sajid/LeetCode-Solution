b903a5270904a0b3572f7d73a6212516d3c16649
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-12-03 16:59:29 +0600

    Update Find Unique .java

--- DSA/Array/Find Unique .java
+++ DSA/Array/Find Unique .java
@@ -5,7 +5,7 @@ public class Solution{
     public static int findUnique(int[] arr){
         Arrays.sort(arr);
         for(int i=0;i<arr.length;i++){
-          if((i+1)<=(arr.length-1) && arr[i]==arr[i+1]){
+          if((i+1)<arr.length && arr[i]==arr[i+1]){
               i=i+1;
           }else{
               return arr[i];

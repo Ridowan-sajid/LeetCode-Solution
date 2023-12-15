86c27551fecea2846bfadc925688b7dea621a765
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-12-16 00:57:23 +0600

    Update Book Allocation.java

--- DSA/Binary Search/Book Allocation.java
+++ DSA/Binary Search/Book Allocation.java
@@ -42,3 +42,53 @@ public class Solution {
         return ans;
     }
 }
+
+
+//////////This one is for, if the array is too long. then we have to handle it by using long and mid should be used like this(given below)/////////////
+
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static boolean isPossible(int n,int m,int[] time,long mid){
+        long count=1;
+        long sum=0;
+
+        for(int k:time){
+            if(sum+k<=mid){
+                sum+=k;
+            }else{
+                count++;
+                if(count>n || k>mid){
+                    return false;
+                }
+                sum=k;
+            }
+        }
+        return true;
+    }
+    public static long ayushGivesNinjatest(int n, int m, int[] time) {
+        long i=0;
+        long j=0;
+        for(int t:time){
+            j+=t;
+        }
+        long ans=-1;
+        long mid=i+(j-i)/2;
+
+
+        while(i<=j){
+            if(isPossible(n,m,time,mid)){
+                ans=mid;
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }   
+            mid=i+(j-i)/2;         
+        }
+        return ans;
+    }
+}
+
+
+

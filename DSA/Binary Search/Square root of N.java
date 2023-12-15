e3db2e948f596a80d3e9ab0d34bbd457b48d3a46
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-12-16 01:46:08 +0600

    Update Square root of N.java

--- DSA/Binary Search/Square root of N.java
+++ DSA/Binary Search/Square root of N.java
@@ -1,30 +1,24 @@
-import java.util.* ;
-import java.io.*; 
-
-public class Solution {
-
-	public static int sqrtN(long N) {
-		long i=0;
-		long j=N;
+class Solution {
+    public int mySqrt(int x) {
+        int i=0;
+		int j=x;
 		int result=-1;
 
 		while(i<=j){
-			long mid=(i+j)/2;
-			if((mid*mid)==N){
-				result=(int)mid;
-				return result;
-			}else if((mid*mid)>N){
-				//result=(int)mid;
+			int mid=(i+j)/2;
+			if((long)mid*mid==(long)x){
+				return mid;
+			}else if((long)mid*mid>(long)x){
 				j=mid-1;
 			}
 			else{
-				result=(int)mid;
+				result=mid;
 				i=mid+1;
 			}
 
 		}
 		return result;
-	}
+    }
 }
 //Why we just add result in ((mid*mid)<N), not for other one.
 //Because we can take the lower value from exact value but

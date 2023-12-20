1aabf5bd4f2d433b59666fd385d8aab0fae8811a
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-12-20 23:47:39 +0600

    Update Peak Index in a Mountain Array.java

--- DSA/Binary Search/Peak Index in a Mountain Array.java
+++ DSA/Binary Search/Peak Index in a Mountain Array.java
@@ -16,3 +16,44 @@ class Solution {
         return -1;
     }
 }
+
+
+/***In Leetcode the question is a little bit different also the answer.**/
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int i=0;
+        int j=nums.length-1;
+        int ans=-1;
+        if(nums.length==1){
+            return 0;
+        }
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if((mid+1)>=nums.length || (mid-1)<0){
+               if((mid-1)<0){
+                if(nums[mid]<nums[mid+1]){
+                    return mid+1;
+                }
+               }
+                return mid;
+            }
+            else{
+                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                    return mid;
+                }
+                else if(nums[mid]>nums[mid-1]){
+                    i=mid+1;
+                }else if(nums[mid]>nums[mid+1]){
+                    j=mid-1;
+                }else if(nums[mid]<nums[mid-1]){
+                    j=mid-1;
+                }else if(nums[mid]<nums[mid+1]){
+                    i=mid+1;
+                }
+            }
+
+        }
+        return ans;
+    }
+}

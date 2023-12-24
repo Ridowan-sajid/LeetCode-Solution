008afa09d689de5a7ca10339523c32ab88879088
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-12-24 20:52:58 +0600

    Update 2. Merge Sort.cs

--- 2. Merge Sort.cs
+++ 2. Merge Sort.cs
@@ -1,20 +1,12 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         int newLength=m+n;
-        int[] arr=new int[newLength];
-        for(int i=0;i<m;i++){
-            arr[i]=nums1[i];
-        }
         int j=0;
         for(int i=m;i<newLength;i++){
-            arr[i]=nums2[j];
+            nums1[i]=nums2[j];
             j++;
         }
-        Array.Sort(arr);
-        for(int i=0;i<arr.Length;i++){
-            nums1[i]=arr[i];
-        }
-        
+        Array.Sort(nums1);      
         
     }
 }

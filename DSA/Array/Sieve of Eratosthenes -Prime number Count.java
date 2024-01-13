fcc9816a1f92bb9a88b7c6302135ab1967790493
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2024-01-14 02:14:23 +0600

    Update Sieve of Eratosthenes -Prime number Count.java

--- DSA/Array/Sieve of Eratosthenes -Prime number Count.java
+++ DSA/Array/Sieve of Eratosthenes -Prime number Count.java
@@ -21,3 +21,6 @@ class Solution {
         return count;
     }
 }
+//Sieve of Eratosthenes: In this way we assume all value is prime then we will check it by adding the base value. Assume n=6; loop will start from 2. Then in inner 
+//loop j=i+i; means 2+2=4;j=i+j; that means we will add 2+j; So, all value which can be divided by 2 they will be count as not prime. this way we can check with
+//3,5 etc.

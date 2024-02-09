c4c39c33ae7855f3d805d05fea69e8f68649cdb3
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2024-02-09 22:04:35 +0600

    Create Basic LinkedList.java

---
+++ DSA/LinkedList/Basic LinkedList.java
@@ -0,0 +1,17 @@
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Node n1=new Node(10);
+
+        System.out.println(n1.data);
+        System.out.println(n1.next);
+    }
+}

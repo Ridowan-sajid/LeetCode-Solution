f5d4d5d6143bf73523932c33d8ca60b300bdc2bb
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2024-03-16 23:37:59 +0600

    Update Reverse Stack Using Recursion.java

--- DSA/Stack/Reverse Stack Using Recursion.java
+++ DSA/Stack/Reverse Stack Using Recursion.java
@@ -1,28 +1,26 @@
 import java.util.Stack;
 
 public class Solution {
-	  public static void solve(Stack <Integer> stack, int x){
-        if(stack.isEmpty()){
-          stack.push(x);
-          return;
-        }
-        int n=stack.peek();
-        stack.pop();
-        solve(stack, x);
-        stack.push(n);
-  }
-    public static void reverse(Stack<Integer> stack){
+	public static void insertBottom(Stack <Integer> stack, int x){
+		if(stack.isEmpty()){
+		  stack.push(x);
+		  return;
+		}
+		int n=stack.peek();
+		stack.pop();
+		insertBottom(stack, x);
+		stack.push(n);
+	}
+	public static void reverse(Stack<Integer> stack){
 		if(stack.isEmpty()){
 			return;
 		}
 		int n=stack.peek();
 		stack.pop();
 		reverse(stack);
-		solve(stack, n);
-
+		insertBottom(stack, n);
 	}
 	public static void reverseStack(Stack<Integer> stack) {
 		reverse(stack);
 	}
-
 }
